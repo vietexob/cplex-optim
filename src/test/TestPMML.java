6d97ac595987ecc1cd7685420ccf8250dcc932d5
Truc Viet Le <vietexob@gmail.com>
2017-02-20 21:45:42 +0800

    Finished JPMML

--- src/test/TestPMML.java
+++ src/test/TestPMML.java
@@ -1,11 +1,26 @@
 package test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.xml.transform.Source;
 
+import org.dmg.pmml.FieldName;
 import org.dmg.pmml.PMML;
+import org.dmg.pmml.regression.RegressionModel;
+import org.jpmml.evaluator.Evaluator;
+import org.jpmml.evaluator.FieldValue;
+import org.jpmml.evaluator.InputField;
+import org.jpmml.evaluator.ModelEvaluator;
+import org.jpmml.evaluator.regression.RegressionModelEvaluator;
 import org.jpmml.model.ImportFilter;
 import org.jpmml.model.JAXBUtil;
 import org.xml.sax.InputSource;
@@ -31,9 +46,39 @@ public class TestPMML {
 	
 	public static void main(String[] args) throws Exception {
 //		System.out.println(System.getProperty("user.dir"));
-		String filename = "./data/test/resp_lm.xml";
-		PMML pmml = loadModel(filename);
-		System.out.println(pmml);
+		String modelFilename = "./data/test/resp_lm.xml";
+		PMML pmml = loadModel(modelFilename);
 		
+		ModelEvaluator<RegressionModel> modelEvaluator = new RegressionModelEvaluator(pmml);
+		Evaluator evaluator = modelEvaluator;
+		List<InputField> features = evaluator.getActiveFields();
+		
+		String testFilename = "./data/test/test_data.csv";
+		BufferedReader br = null;
+		String line = "";
+		try {
+			br = new BufferedReader(new FileReader(testFilename));
+			String firstLine = br.readLine();
+			while((line = br.readLine()) != null) {
+				String[] lineStr = line.split(",");
+				HashMap<FieldName, Double> featureRow = new HashMap<FieldName, Double>();
+				
+				for(int i = 0; i < lineStr.length-1; i++) {
+					double featureVal = Double.valueOf(lineStr[0]);
+					FieldName featureName = features.get(i).getName();
+					featureRow.put(featureName, featureVal);
+				}
+				
+				Map results = modelEvaluator.evaluate(featureRow);
+				FieldName key = evaluator.getTargetFields().get(0).getName();
+				System.out.println(results.get(key));
+			}
+		} catch(FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			br.close();
+		}
 	}
 }
