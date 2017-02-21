67aed27dbe2415d30a71fd8e17c2952423b3df2c
Truc Viet Le <vietexob@gmail.com>
2017-02-21 12:25:32 +0800

    Finished JPMML

--- src/test/TestPMML.java
+++ src/test/TestPMML.java
@@ -7,7 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,13 +21,14 @@ import org.jpmml.evaluator.Evaluator;
 import org.jpmml.evaluator.FieldValue;
 import org.jpmml.evaluator.InputField;
 import org.jpmml.evaluator.ModelEvaluator;
+import org.jpmml.evaluator.TargetField;
 import org.jpmml.evaluator.regression.RegressionModelEvaluator;
 import org.jpmml.model.ImportFilter;
 import org.jpmml.model.JAXBUtil;
 import org.xml.sax.InputSource;
 
 public class TestPMML {
-	// Test reading a trained model saved as PMML into a Java program
+	// Test reading a trained model saved as PMML into a Java program and do prediction.
 	
 	public final static PMML loadModel(final String filename) throws Exception {
 		PMML pmml = null;
@@ -36,42 +38,59 @@ public class TestPMML {
 			Source source = ImportFilter.apply(new InputSource(in));
 			pmml = JAXBUtil.unmarshalPMML(source);
 		} catch( Exception ex ) {
-//			logger.error(ex.toString());
-			System.out.println(ex.toString());
-			throw ex;
+			ex.printStackTrace();
 		}
 		
 		return pmml;
 	}
 	
 	public static void main(String[] args) throws Exception {
-//		System.out.println(System.getProperty("user.dir"));
 		String modelFilename = "./data/test/resp_lm.xml";
 		PMML pmml = loadModel(modelFilename);
 		
 		ModelEvaluator<RegressionModel> modelEvaluator = new RegressionModelEvaluator(pmml);
-		Evaluator evaluator = modelEvaluator;
-		List<InputField> features = evaluator.getActiveFields();
+		Evaluator evaluator = (Evaluator) modelEvaluator;
 		
 		String testFilename = "./data/test/test_data.csv";
 		BufferedReader br = null;
 		String line = "";
+		List<Double> testVal = new LinkedList<Double>();
+		List<Double> predVal = new LinkedList<Double>();
+		
 		try {
 			br = new BufferedReader(new FileReader(testFilename));
 			String firstLine = br.readLine();
+			System.out.println(firstLine);
+			
 			while((line = br.readLine()) != null) {
 				String[] lineStr = line.split(",");
-				HashMap<FieldName, Double> featureRow = new HashMap<FieldName, Double>();
+				Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
 				
-				for(int i = 0; i < lineStr.length-1; i++) {
-					double featureVal = Double.valueOf(lineStr[0]);
-					FieldName featureName = features.get(i).getName();
-					featureRow.put(featureName, featureVal);
+				List<InputField> inputFields = evaluator.getInputFields();
+				for(int i = 0; i < inputFields.size()+1; i++) {
+					if(i < inputFields.size()) {
+						InputField inputField = inputFields.get(i);
+						FieldName inputFieldName = inputField.getName();
+						Double featureVal = Double.valueOf(lineStr[i]);
+						
+						FieldValue inputFieldValue = inputField.prepare(featureVal);
+						arguments.put(inputFieldName, inputFieldValue);
+					} else {
+						Double aTestVal = Double.valueOf(lineStr[i]);
+						testVal.add(aTestVal);
+					}
 				}
 				
-				Map results = modelEvaluator.evaluate(featureRow);
-				FieldName key = evaluator.getTargetFields().get(0).getName();
-				System.out.println(results.get(key));
+				Map<FieldName, ?> results = evaluator.evaluate(arguments);
+				List<TargetField> targetFields = evaluator.getTargetFields();
+				for(TargetField targetField : targetFields) {
+					FieldName targetFieldName = targetField.getName();
+					Double targetFieldValue = (Double) results.get(targetFieldName);
+					if(targetFieldValue < 1.0) {
+						targetFieldValue = 1.0;
+					}
+					predVal.add(targetFieldValue);
+				}
 			}
 		} catch(FileNotFoundException ex) {
 			ex.printStackTrace();
@@ -79,6 +98,34 @@ public class TestPMML {
 			ex.printStackTrace();
 		} finally {
 			br.close();
+		}
+		
+		double rmse = getPredError(predVal, testVal, true);
+		double mae = getPredError(predVal, testVal, false);
+		System.out.println("RMSE = " + rmse + ", MAE = " + mae);
+	}
+	
+	public static double getPredError(List<Double> predVal, List<Double> testVal, boolean isRMSE) {
+		if(isRMSE) {
+			double rmse = 0;
+			double diff;
+			for(int i = 0; i < predVal.size(); i++) {
+				diff = testVal.get(i) - predVal.get(i);
+				rmse += diff * diff;
+			}
+			
+			rmse = Math.sqrt(rmse / predVal.size());
+			return rmse;
+		} else {
+			double mae = 0;
+			double diff;
+			for(int i = 0; i < predVal.size(); i++) {
+				diff = testVal.get(i) - predVal.get(i);
+				mae += Math.abs(diff);
+			}
+			
+			mae = mae / predVal.size();
+			return mae;
 		}
 	}
 }
